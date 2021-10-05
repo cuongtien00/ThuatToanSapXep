ad9b2427eb6e0263792661df8369c087c247220d
cuongtien00 <cuonghiep000@gmail.com>
2021-10-05 11:54:01 +0700

    add SettingInsertSort

---
+++ src/SettingInsertSort/Main.java
@@ -0,0 +1,25 @@
+package SettingInsertSort;
+
+public class Main {
+        public static void main(String[] args) {
+            int [] arr = {1,5,3,7,9,2,8};
+            insertSort(arr);
+
+        }
+        public static void insertSort(int[]arr){
+            for (int i = 1; i <arr.length ; i++) {
+                int currentE = arr[i];
+                int j;
+                for (j = i-1; j >=0 && arr[j]>currentE ; j--) {
+                    arr[j+1] = arr[j];
+                }
+                arr[j+1] = currentE;
+            }
+            for (int a:arr
+            ) {
+                System.out.println(a);
+
+            }
+        }
+    }
+
